976f8c0fe5abb964b5a646cf1d079895bc715d89
猴子 <a102009102009@gmail.com>
2022-10-10 22:24:23 +0800

    Update BaseHeader.vue

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -6,8 +6,14 @@
 
 <style lang="scss" scoped>
 header {
+  position: fixed;
+  top: 0;
   width: 100%;
   height: 60px;
   background: linear-gradient(#c6cb6b92, transparent);
+}
+
+:global(body) {
+  margin-top: 60px;
 }
 </style>
