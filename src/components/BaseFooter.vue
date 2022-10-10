c1ada8ea334bea2bdc0cb0a4c79de2d9376942a5
猴子 <a102009102009@gmail.com>
2022-10-10 21:58:56 +0800

    feat: edit footer

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,76 +1,147 @@
 <template>
   <footer>
-    <h1>小鹿樹教育文化協會</h1>
+    <!-- TODO div -->
 
-    <ul class="info">
-      <!-- email -->
-      <li itemprop="email" aria-label="Email: little.dear.tree2020@gmail.com">
-        <SvgIcon name="connection-mail" size="22px" color="#8b949e" />
-        <a href="mailto:little.dear.tree2020@gmail.com">
-          little.dear.tree2020@gmail.com
-        </a>
-      </li>
+    <div class="base-info">
+      <h1 class="name">小鹿樹教育文化協會</h1>
 
-      <!-- facebook -->
-      <li
-        itemprop="facebook"
-        aria-label="facebook: 小鹿樹教育文化協會/Little.Dear.Tree"
-      >
-        <SvgIcon name="connection-facebook" size="22px" color="#8b949e" />
-        <a
-          href="https://www.facebook.com/%E5%B0%8F%E9%B9%BF%E6%A8%B9%E6%95%99%E8%82%B2%E6%96%87%E5%8C%96%E5%8D%94%E6%9C%83LittleDearTree-101351098369983/"
+      <div class="community">
+        <!-- email -->
+        <span
+          itemprop="email"
+          aria-label="Email: little.dear.tree2020@gmail.com"
         >
-          小鹿樹教育文化協會/Little.Dear.Tree
-        </a>
-      </li>
+          <SvgIcon name="connection-mail" size="22px" color="#8b949e" />
+          <a href="mailto:little.dear.tree2020@gmail.com">
+            little.dear.tree2020@gmail.com
+          </a>
+        </span>
 
-      <!-- instagram -->
-      <li itemprop="instagram" aria-label="instagram: little.dear.tree2020">
-        <SvgIcon name="connection-instagram" size="22px" color="#8b949e" />
-        <a href="https://www.instagram.com/little.dear.tree2020">
-          little.dear.tree2020
-        </a>
-      </li>
-    </ul>
+        <!-- facebook -->
+        <span
+          itemprop="facebook"
+          aria-label="facebook: 小鹿樹教育文化協會/Little.Dear.Tree"
+        >
+          <SvgIcon name="connection-facebook" size="22px" color="#8b949e" />
+          <a
+            href="https://www.facebook.com/%E5%B0%8F%E9%B9%BF%E6%A8%B9%E6%95%99%E8%82%B2%E6%96%87%E5%8C%96%E5%8D%94%E6%9C%83LittleDearTree-101351098369983/"
+          >
+            小鹿樹教育文化協會/Little.Dear.Tree
+          </a>
+        </span>
+
+        <!-- instagram -->
+        <span itemprop="instagram" aria-label="instagram: little.dear.tree2020">
+          <SvgIcon name="connection-instagram" size="22px" color="#8b949e" />
+          <a href="https://www.instagram.com/little.dear.tree2020">
+            little.dear.tree2020
+          </a>
+        </span>
+      </div>
+    </div>
+    <div class="else-info">
+      <div>
+        <h2>了解更多</h2>
+        <ul>
+          <li><a href="">近期活動</a></li>
+          <li><a href="">人才招募</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2>支持我們</h2>
+        <ul>
+          <li><a href="">捐款計畫</a></li>
+          <li><a href="">捐款徵信</a></li>
+          <li><a href="">感謝名單</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2>佔位</h2>
+        <ul>
+          <li><a href="">佔位</a></li>
+          <li><a href="">佔位</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2>佔位</h2>
+        <ul>
+          <li><a href="">佔位</a></li>
+          <li><a href="">佔位</a></li>
+          <li><a href="">佔位</a></li>
+        </ul>
+      </div>
+    </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
-ul {
-  li {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
+footer {
+  display: grid;
+  padding: 1em 2em;
+  color: white;
+  background-color: black;
+  grid-template-columns: 1fr 2fr;
 
-    a {
-      margin-left: 5px;
-      color: #c9d1d9;
+  @media (max-width: 950px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 750px) {
+    grid-template-columns: initial;
 
-      &:hover {
-        color: rgb(87 161 244);
-      }
+    .else-info {
+      grid-row-start: -1;
     }
   }
 }
 
-footer {
-  padding: 1em 2em;
-  background-color: black;
+.else-info {
+  gap: 15px;
+  display: grid;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+
+  > div {
+    margin-bottom: 2rem;
+  }
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+}
+
+.base-info {
+  display: flex;
+  margin-bottom: 10px;
+  flex-direction: column;
+}
+
+.community {
+  display: flex;
+  flex-direction: column;
+
+  span {
+    margin-top: 8px;
+  }
 }
 
 h1 {
   position: relative;
-  margin-bottom: 1em;
+  margin-bottom: 8px;
+  font-size: 24px;
   color: white;
+}
 
-  &::after {
-    position: absolute;
-    top: 21px;
-    right: 0;
-    width: calc(100% - 320px);
-    height: 1px;
-    background: #61749f;
-    content: '';
+h2 {
+  margin-bottom: 8px;
+  font-size: 18px;
+  color: white;
+}
+
+a {
+  margin-left: 5px;
+  color: #c9d1d9;
+
+  &:hover {
+    color: rgb(87 161 244);
   }
 }
 </style>
