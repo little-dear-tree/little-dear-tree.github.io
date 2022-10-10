7398fd989b2d44fa4d6bdf152a4067c96c4ef308
猴子 <a102009102009@gmail.com>
2022-10-10 22:17:05 +0800

    Update BaseFooter.vue

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -87,20 +87,23 @@ footer {
   }
   @media (max-width: 750px) {
     grid-template-columns: initial;
-
-    .else-info {
-      grid-row-start: -1;
-    }
   }
 }
 
 .else-info {
-  gap: 15px;
   display: grid;
+  justify-items: center;
+  gap: 15px;
   grid-template-columns: repeat(4, minmax(0, 1fr));
 
   @media (max-width: 1000px) {
+    margin-left: 10px;
+    justify-items: normal;
     grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+  @media (max-width: 750px) {
+    margin: initial;
+    grid-row-start: -1;
   }
 }
 
