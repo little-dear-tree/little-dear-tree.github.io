1540c7856c86388340afe382e87e6f95733c9bc8
猴子 <a102009102009@gmail.com>
2022-10-11 19:34:29 +0800

    Update BaseHeader.vue

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -8,9 +8,11 @@
 header {
   position: fixed;
   top: 0;
+  z-index: 999999;
   width: 100%;
   height: 60px;
-  background: linear-gradient(#c6cb6b92, transparent);
+  background-color: white;
+  box-shadow: 0 0 90px 2px #0005;
 }
 
 :global(body) {
