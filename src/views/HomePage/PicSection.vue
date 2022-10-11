e1d375faaf0c9fa1713994b6138261b90b677706
猴子 <a102009102009@gmail.com>
2022-10-11 18:35:54 +0800

    Update PicSection.vue

--- src/views/HomePage/PicSection.vue
+++ src/views/HomePage/PicSection.vue
@@ -21,15 +21,23 @@ const slideshowData: string[] = baseSlideshowData;
       <swiper-slide v-for="(img, index) in slideshowData" :key="index">
         <div lazy-load :style="{ '--image-url': `url(${img})` }"></div>
       </swiper-slide>
+      <div class="container">
+        <h1>
+          Little.Dear.Tree
+          <br />
+          讓每個孩子無所畏懼的說自己的生命故事
+        </h1>
+      </div>
     </swiper>
   </section>
 </template>
 
 <style lang="scss" scoped>
 .pics-slide-block {
+  position: relative;
   display: flex;
   width: 100%;
-  height: 85vh;
+  height: 720px;
   max-height: 950px;
 
   .swiper-slide {
@@ -45,6 +53,27 @@ const slideshowData: string[] = baseSlideshowData;
       background-image: var(--image-url);
       background-position: center;
       background-size: cover;
+    }
+  }
+
+  .container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+
+    h1 {
+      padding-bottom: 8%;
+      font-size: 3rem;
+      line-height: 60px;
+      color: white;
+      text-shadow: 0 0 5px #000;
     }
   }
 }
