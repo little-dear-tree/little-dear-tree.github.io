026e7c2c09be77c46b03fb06b2732d6fea770229
猴子 <a102009102009@gmail.com>
2022-10-11 19:56:36 +0800

    Update BaseHeader.vue

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -10,12 +10,12 @@ header {
   top: 0;
   z-index: 999999;
   width: 100%;
-  height: 60px;
+  height: 5rem;
   background-color: white;
   box-shadow: 0 0 90px 2px #0005;
 }
 
 :global(body) {
-  margin-top: 60px;
+  margin-top: 5rem;
 }
 </style>
